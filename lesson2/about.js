63fdc13cb43a97e42bcb75faf8c56b78278cb957
andreyantar <antar78@gmail.com>
2025-01-21 23:09:50 +0200

    Add lesson2 files

---
+++ lesson2/about.js
@@ -0,0 +1,6 @@
+const name = "Андрей";
+const age = 75;
+const city = "Балтийск";
+const hobby = "дизайн и программирование";
+
+console.log(`Меня зовут ${name}, мне ${age} лет, я живу в городе ${city} и увлекаюсь ${hobby}.`);
