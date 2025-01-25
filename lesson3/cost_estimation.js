09c76c4554fe61e1cca608f4921e5dd3300ceca5
andreyantar <antar78@gmail.com>
2025-01-25 17:56:23 +0200

    Update cost_estimation.js with new values

---
+++ lesson3/cost_estimation.js
@@ -0,0 +1,6 @@
+const titleProject = "Оценка стоимости сайта";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 4500;
+const percentage = 15;
+const fullPrice = 100000;
+const responsive = true;
