e9b6691de753c88f78cb4827f3ebd6eccad29dd0
andreyantar <antar78@gmail.com>
2025-02-01 22:21:16 +0200

    Remove lessons 1–3 from lesson4 branch

--- lesson3/cost_estimation.js
+++
@@ -1,7 +0,0 @@
-const titleProject = "Оценка стоимости сайта";
-const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
-const screenPrice = 4500;
-const percentage = 15;
-const fullPrice = 100000;
-const responsive = true;
-console.log(titleProject);
